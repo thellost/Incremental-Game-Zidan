0c57d3fd561502f935b062810417aa510a9f273c
thellost <aldy10ball@gmail.com>
2021-09-09 17:28:43 +0700

    add additional feature
    add resource
    add clicking feature based on how much resource we have

---
+++ Incremental Project/Assets/Scripts/TapArea.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.EventSystems;
+
+public class TapArea : MonoBehaviour , IPointerDownHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+
+    {
+        Debug.Log("test tap");
+
+        GameManager.Instance.CollectByTap(eventData.position, transform);
+
+    }
+}
---
+++ Incremental Project/Assets/Scripts/TapText.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class TapText : MonoBehaviour
+{
+
+    public float SpawnTime = 0.5f;
+
+    public TextMeshProUGUI Text;
+    private float _spawnTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+
+    {
+
+        _spawnTime = SpawnTime;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _spawnTime -= Time.unscaledDeltaTime;
+
+        if (_spawnTime <= 0f)
+
+        {
+
+            gameObject.SetActive(false);
+
+        }
+
+        else
+
+        {
+
+            Text.CrossFadeAlpha(0f, 0.5f, false);
+
+            if (Text.color.a == 0f)
+
+            {
+
+                gameObject.SetActive(false);
+
+            }
+
+        }
+    }
+}
